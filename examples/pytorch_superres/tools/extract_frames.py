3045da0c4fc46be3efc831214737de133b7e420e
Jared Casper <jcasper@nvidia.com>
2018-04-16 11:15:13 -0700

    extract_frames from transcoded scenes. add --quiet.

--- examples/pytorch_superres/tools/extract_frames.py
+++ examples/pytorch_superres/tools/extract_frames.py
@@ -2,43 +2,111 @@ import argparse
 import os
 import subprocess
 
-def extract_frames(master_path, resolution, codec, crf, keyint):
+default_format = "png"
+default_qscale_jpg = "4"
 
-    desc = [resolution, 'scenes']
+def extract_frames(master_data, resolution, format, q, quiet,
+                   transcoded, codec, crf, keyint):
+
+    if transcoded:
+        desc = [resolution, 'scenes']
+        desc += [codec] if codec else []
+        desc += ["crf"+crf] if crf else []
+        desc += ["keyint"+keyint] if keyint else []
+        in_path = os.path.join(master_data,*desc)
+    else:
+        if codec:
+            raise ValueError("--codec specified, but not --transcoded");
+        if crf:
+            raise ValueError("--crf specified, but not --transcoded");
+        if keyint:
+            raise ValueError("--keyint specified, but not --transcoded");
+        in_path = os.path.join(master_data,'orig','scenes')
+
+    desc = [resolution,'frames']
     desc += [codec] if codec else []
     desc += ["crf"+crf] if crf else []
     desc += ["keyint"+keyint] if keyint else []
-    master_in_path = os.path.join(master_path,*desc)
+    if not format:
+        format = default_format
+    else:
+        desc += [format]
 
-    if not os.path.exists(os.path.join(master_in_path,'train')):
-        raise ValueError("No training data found in "+master_in_path+".\n" +
-                         "If you specified a codec, crf, or keyint in\n" +
-                         "transcode_scenes.py you should do so as well here.")
+    if not q:
+        if format == "jpg":
+            q = default_qscale_jpg
+    else:
+        desc += ["q" + q]
 
-    for subset in ['train', 'val']:
-        for in_file in os.listdir(os.path.join(master_in_path,subset)):
+    out_path = os.path.join(master_data,*desc)
+
+    res_args = []
+    if resolution == '4K':
+        pass
+    else:
+        if resolution == '1080p':
+            res_str = '1920:1080'
+        elif resolution == '720p':
+            res_str = '1280:720'
+        elif resolution == '540p':
+            res_str = '960:540'
+        else:
+            raise ValueError("Unknown resolution")
+        res_args += ["-vf", "scale=%s" % res_str, "-sws_flags", "bilinear"]
+
+    codec_args = []
+    if format == "png":
+        if q:
+            codec_args += ["-compression_level", q]
+    elif format == "jpg":
+        codec_args += ["-q:v", q]
+    else:
+        raise ValueError("Unknown format")
+
+    if quiet:
+        cmdout = subprocess.DEVNULL
+    else:
+        cmdout = None
+
+    for subset_name, subset_dir in [('training', 'train'), ('validation', 'val')]:
+        if not os.path.exists(os.path.join(in_path,subset_dir)):
+            raise ValueError("No "+subset_name+" data found in "+in_path+", " +
+                             "did you run split_scenes.py?")
+
+        for in_file in os.listdir(os.path.join(in_path,subset_dir)):
             if in_file.endswith('.mp4'):
                 scene = in_file.split('_')[1].split('.')[0]
-                out_path = os.path.join(master_path,resolution,'frames',subset,scene)
-                if not os.path.isdir(out_path):
-                    os.makedirs(out_path)
-                in_path = os.path.join(master_in_path,subset,in_file)
-                cmd = ["ffmpeg", "-i", in_path, os.path.join(out_path, "%05d.png")]
-                subprocess.run(cmd)
+                cur_out_path = os.path.join(out_path,subset_dir,scene)
+                if not os.path.isdir(cur_out_path):
+                    os.makedirs(cur_out_path)
+                cur_in_path = os.path.join(in_path,subset_dir,in_file)
+                cmd = ["ffmpeg", "-n", "-i", cur_in_path]
+                cmd += res_args
+                cmd += codec_args
+                cmd += [os.path.join(cur_out_path, "%05d."+format)]
+                print("Running:", " ".join(cmd))
+                subprocess.run(cmd, stdout=cmdout, stderr=cmdout)
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--master_path', type=str, default=None, required=True,
+    parser.add_argument('--master_data', type=str, required=True,
                         help="Path to root data directory")
-    parser.add_argument('--resolution', type=str, default=None, required=True,
-                        help="one of '4K', '1080p', '720p', or '540p'")
+    parser.add_argument('--resolution', type=str, required=True,
+                        choices=['4K', '1080p', '720p', '540p'])
+    parser.add_argument('--format', type=str,
+                        choices=['png', 'jpg'])
+    parser.add_argument('-q', type=str,
+                        help="quality to use for compression [2-31] for jpg and [0-9] for png")
+    parser.add_argument('--transcoded', action='store_true',
+                        help="Use transcoded videos instead of original split video")
+    parser.add_argument('--quiet', action='store_true',
+                        help="Suppress ffmpeg output")
     parser.add_argument('--codec', type=str, default=None,
-                        help="one of 'h264' or 'hevc'")
+                        choices=['h264', 'hevc'],
+                        help="codec of transcoded video to use")
     parser.add_argument('--crf', type=str, default=None,
-                        help="crf value passed to ffmpeg")
+                        help="crf value of transcoded video to use")
     parser.add_argument('--keyint', type=str, default=None,
-                        help="keyframe interval")
+                        help="keyframe interval of transcoded video to use")
     args = parser.parse_args()
-    assert args.resolution in ['1080p', '720p', '540p'], '--resolution must be one of 1080p, 720p, 540p'
-    assert args.master_path is not None, 'Provide --master_data path to root data directory containing Myanmar scenes in desired resolution'
     extract_frames(**vars(args))
--- examples/pytorch_superres/tools/transcode_scenes.py
+++ examples/pytorch_superres/tools/transcode_scenes.py
@@ -6,7 +6,7 @@ default_codec = "h264"
 default_crf = "18"
 default_keyint = "4"
 
-def downsample_scenes(master_data, resolution, codec, crf, keyint):
+def downsample_scenes(master_data, resolution, codec, crf, keyint, quiet):
 
     desc = [resolution, 'scenes']
     if not codec:
@@ -49,7 +49,7 @@ def downsample_scenes(master_data, resolution, codec, crf, keyint):
 
     codec_args = ["-preset", "slow"]
     if codec == 'h264':
-        codec_args = ["-c:v", "libx264", "-g", keyint, "-keyint_min", keyint,
+        codec_args = ["-c:v", "libx264", "-g", keyint,
                       "-profile:v", "high"]
     elif codec == 'hevc' or codec == 'h265':
         codec_args = ["-c:v", "libx265", "-x265-params",
@@ -57,13 +57,18 @@ def downsample_scenes(master_data, resolution, codec, crf, keyint):
     else:
         raise ValueError("Unknown codec")
 
+    if quiet:
+        cmdout = subprocess.DEVNULL
+    else:
+        cmdout = None
+
     def transcode(in_path, out_path):
-            cmd = ["ffmpeg", "-i", in_path]
-            cmd += res_args
-            cmd += codec_args
-            cmd += ["-crf", crf, "-an", out_path]
-            print("Running:", " ".join(cmd))
-            subprocess.run(cmd)
+        cmd = ["ffmpeg", "-y", "-i", in_path]
+        cmd += res_args
+        cmd += codec_args
+        cmd += ["-crf", crf, "-an", out_path]
+        print("Running:", " ".join(cmd))
+        subprocess.run(cmd, stdout=cmdout, stderr=cmdout)
 
     for subset in ['train', 'val']:
         for in_file in os.listdir(os.path.join(master_data,'orig','scenes',subset)):
@@ -84,6 +89,8 @@ if __name__=='__main__':
                         help="crf value passed to ffmpeg")
     parser.add_argument('--keyint', type=str, default=None,
                         help="keyframe interval")
+    parser.add_argument('--quiet', action='store_true',
+                        help="Suppress ffmpeg output")
     args = parser.parse_args()
     assert args.master_data is not None, 'Provide --master_data path to root data directory containing split scenes'
     assert args.resolution in ['4K', '1080p', '720p', '540p'], '--resolution must be one of 1080p, 720p, 540p'
